911208053f3f0303f651575390380b9240225661
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-11-26 17:15:07 +0900

    ログイン関連の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    update_attrs = [:password, :password_confirmation, :current_password, :username]
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: update_attrs)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: true },
+            format: { with: /\A[ぁ-んァ-ン０-９a-zA-Z0-9一-龥]+\z/ }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :username, presence: true,
-                       uniqueness: { case_sensitive: true },
-                       format: { with: /\A[ぁ-んァ-ン０-９a-zA-Z0-9一-龥\- 　]+z/}
 end
