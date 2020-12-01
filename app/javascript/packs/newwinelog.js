ed65480e950488c731987e095f9748203ac80686
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-12-01 19:19:30 +0900

    vue導入実験

--- app/javascript/packs/newwinelog.js
+++ app/javascript/packs/newwinelog.js
@@ -1,7 +1,17 @@
-// var app = new Vue({
-//   el: ''
-// })
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
 
-window.addEventListener("load", () => {
-  console.log("OK");
-});
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener('turbolinks:load', () => {
+  const app = new Vue({
+    el: '#app',
+    data: {
+      message: "Can you say hello?",
+      wine: "Gevrey Chambertin",
+      selected: ''
+    }
+    // components: { App }
+  })
+})
