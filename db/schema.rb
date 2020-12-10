3e371b1d30b84c823e54144a99b3eac922d928d5
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-12-10 18:39:33 +0900

    Revert "default_templateの廃止"
    This reverts commit e8e572e189fee6324237dcf8a06af5d715083a66.

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,30 @@ ActiveRecord::Schema.define(version: 2020_12_05_044647) do
     t.index ["default_country_id"], name: "index_default_region1s_on_default_country_id"
   end
 
+  create_table "default_templates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "template_name", null: false
+    t.integer "sort_order"
+    t.integer "type_id_value"
+    t.integer "country_id_value"
+    t.integer "region1_id_value"
+    t.string "region2_value"
+    t.string "region3_value"
+    t.string "producer_value"
+    t.integer "class_id_value"
+    t.integer "vintage_value"
+    t.string "grape1_value"
+    t.string "grape2_value"
+    t.string "grape3_value"
+    t.string "grape4_value"
+    t.string "grape5_value"
+    t.string "shop_value"
+    t.text "comment_value"
+    t.integer "alcohol_value"
+    t.string "importer_name_value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "default_types", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.integer "sort_order", null: false
