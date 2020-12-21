bc2f799b272723ad0f15f512ab470484a31a1e43
lynxlevin <72173230+lynxlevin@users.noreply.github.com>
2020-12-21 12:45:06 +0900

    Delete 20201211010657_rename_importer_name_value_column_to_original_templates.rb

--- db/migrate/20201211010657_rename_importer_name_value_column_to_original_templates.rb
+++
@@ -1,5 +0,0 @@
-class RenameImporterNameValueColumnToOriginalTemplates < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :original_templates, :importer_name_value, :importer_value
-  end
-end
